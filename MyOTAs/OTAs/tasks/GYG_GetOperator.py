6b91ff7548e95c4153466026c233ecc1a35c27f0
Drink-app <wojbal3@gmail.com>
2024-10-11 10:05:40 +0200

    modified:   MyOTAs/OTAs/scrapers/__pycache__/scraper_gyg.cpython-311.pyc 	new file:   MyOTAs/OTAs/tasks/GYG_GetOperator.py 	new file:   MyOTAs/OTAs/tasks/Musement_GetOperator.py

---
+++ MyOTAs/OTAs/tasks/GYG_GetOperator.py
@@ -0,0 +1,84 @@
+# %%
+
+import pandas as pd
+import os
+import sys
+
+# Set the current directory to the script location
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Add the root directory (project directory) to sys.path
+project_root = os.path.abspath(os.path.join(current_dir, '..'))
+sys.path.append(project_root)
+
+from scrapers.scraper_gyg import ScraperGYG
+from file_management.file_path_manager import FilePathManager, DetermineDebugRun
+from logger.logger_manager import LoggerManager
+
+
+# %%
+
+# %%
+site = "GYG"
+file_manager = FilePathManager(site, 'N/A')
+logger = LoggerManager(file_manager, application="getoperator")
+#
+# css_selectors = {
+#     'provider': 'div[data-test-id*="activity-provider"]',
+# }
+class_selectors = {
+    'provider': 'supplier-name__link',
+}
+
+file_path_xlsx_operator = file_manager.get_file_paths()['file_path_xlsx_operator']
+
+# %%
+
+df = pd.read_excel(file_path_xlsx_operator)
+logger.logger_info.info(f"There are {len(df[df['Operator'] == 'ToDo'])}")
+counter = 1
+df_todo = df[df['Operator'] == 'ToDo']
+for index, row in df_todo.iterrows():
+    
+    url = row['Link']
+    # Log the current row being processed
+    logger.logger_info.info(f"Processing row {index} with URL: {url}")
+
+    if row['Operator'] != "ToDo":
+        logger.logger_info.info(f"Skipping row {index} as the URL is not 'ToDo'.")
+        continue
+    try:
+        scraper = ScraperGYG(url, "N/A", class_selectors, file_manager, logger, provider=True)
+        
+        # Log the initiation of the scraping process
+        logger.logger_info.info(f"Initialized scraper for URL: {url}")
+        scraper.get_url()
+        
+        provider_name = scraper.get_provider_name()
+        provider_name = provider_name.text
+        # Log that the provider name was successfully fetched
+        logger.logger_done.info(f"Provider name fetched for row {index}: {provider_name}")
+            
+        df.at[index, 'Operator'] = provider_name
+        counter += 1
+        if counter % 50 == 0:
+            logger.logger_done.info(f"Already process {counter} saving progress.")
+            scraper.save_dataframe(df, file_path_xlsx_operator)
+    except Exception as e:
+        # Log any errors encountered during the scraping process
+        logger.logger_err.error(f"Error processing row {index} with URL {url}: {str(e)}")
+    finally:
+        # Ensure that the driver is closed
+        # scraper.quit_driver()
+        logger.logger_done.info(f"Closed scraper for URL: {url}")
+
+scraper.save_dataframe(df, file_path_xlsx_operator)
+
+
+# %%
+counter
+
+# %%
+
+
+
---
+++ MyOTAs/OTAs/tasks/Musement_GetOperator.py
@@ -0,0 +1,88 @@
+import os
+import sys
+
+# Set the current directory to the script location
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Add the root directory (project directory) to sys.path
+project_root = os.path.abspath(os.path.join(current_dir, '..'))
+sys.path.append(project_root)
+
+from scrapers.scraper_musement import ScraperMusement
+from file_management.file_path_manager import FilePathManager, DetermineDebugRun
+from logger.logger_manager import LoggerManager
+import pandas as pd
+
+# %%
+
+
+# %%
+site = "Musement"
+css_selectors = {
+    'currency': 'div[data-test*="dropdown-currency"]',
+    'currency_list': 'section[class*="row-start-center"]',
+    'products_count': 'span[data-test-id*="search-component-activity-count-text"]',
+    'view_more_button': 'button[data-test-id="search-component-test-btn"]',
+    'show_more_button': 'a[data-qa-marker*="loading-button"]',
+    'product_card': 'div[data-test*="ActivityCard"]',
+    'tour_price': 'span[data-test="realPrice"]',
+    'tour_price_discount': 'div[class="tour-scratch-price"]',
+    'ratings': 'div[data-test="reviewTest"]',
+    'review_count': 'p[class*="reviewsNumber"]',
+    'category_label': 'div[data-test="main-category"]',
+    'js_script_for_shadow_root': 'return document.querySelector("msm-cookie-banner").shadowRoot',
+    'cookies_banner': 'button[data-test*="decline-cookies"]',
+    'sort_by': 'select[data-test-id="search-component-sort-selector"]',
+    'option_rating': 'option[value*="rating"]',
+    'option_popularity': 'option[value*="relevance-city"]',
+    'provider': 'div[class*="src-shared_component-blockBody"]'
+}
+
+
+# %%
+file_manager = FilePathManager(site, "NA")
+logger = LoggerManager(file_manager)
+file_path_xlsx_operator = file_manager.get_file_paths()['file_path_xlsx_operator']
+
+
+# %%
+
+df = pd.read_excel(file_path_xlsx_operator)
+counter = 1
+for index, row in df.iterrows():
+    
+    url = row['Link']
+    # Log the current row being processed
+    logger.logger_info.info(f"Processing row {index} with URL: {url}")
+
+    if row['Operator'] != "ToDo":
+        logger.logger_info.info(f"Skipping row {index} as the URL is not 'ToDo'.")
+        continue
+    try:
+        scraper = ScraperMusement(url, None, css_selectors,  file_manager, logger, provider=True)
+        # Log the initiation of the scraping process
+        logger.logger_info.info(f"Initialized scraper for URL: {url}")
+        scraper.get_url()
+        
+        provider_name = scraper.get_provider_name()
+        if provider_name == 'Not Found':
+            logger.logger_done.info(f"Provider not found for row {index}: url: {url}")
+            continue
+        # Log that the provider name was successfully fetched
+        logger.logger_done.info(f"Provider name fetched for row {index}: {provider_name.text}")
+            
+        df.at[index, 'Operator'] = provider_name.text
+        counter += 1
+        if counter % 50 == 0:
+            logger.logger_done.info(f"Already process {counter} saving progress.")
+            scraper.save_dataframe(df, file_path_xlsx_operator)
+    except Exception as e:
+        # Log any errors encountered during the scraping process
+        logger.logger_err.error(f"Error processing row {index} with URL {url}: {str(e)}")
+    finally:
+        # Ensure that the driver is closed
+        scraper.quit_driver()
+        logger.logger_done.info(f"Closed scraper for URL: {url}")
+
+scraper._save_dataframe(df)
+
