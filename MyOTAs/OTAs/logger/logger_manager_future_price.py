3ba666484779c2f1256e98e517aa0cd4cf40f062
MyOTAs_Tech_WB <wojbal3@gmail.com>
2025-01-28 11:00:22 +0100

    modified:   MyOTAs/OTAs/logger/logger_manager_future_price.py

--- MyOTAs/OTAs/logger/logger_manager_future_price.py
+++ MyOTAs/OTAs/logger/logger_manager_future_price.py
@@ -14,24 +14,31 @@ from logger.logger_manager import LoggerManager
 class LoggerManagerFuturePrice(LoggerManager):
     def __init__(self, file_manager, application="future_price"):
         super().__init__(file_manager, application)
-         # Use the formatter from the base class
+        
+        # Use the formatter from the base class
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
+        # Set up the log path
         current_log_path = self.get_current_log_path()
+
+        # Create and configure the 'Statistics_logger'
         self.logger_statistics = logging.getLogger('Statistics_logger')
         self.logger_statistics.setLevel(logging.DEBUG)
+
+        # Create a stream handler for the logger
+        self.ch = logging.StreamHandler()  # Add this line to define the missing attribute
+        self.ch.setLevel(logging.DEBUG)
+        self.ch.setFormatter(formatter)
+
+        # Create a file handler for the logger
         self.fh_statistics = logging.FileHandler(os.path.join(current_log_path, f'{application}_statistics_logs.log'))
         self.fh_statistics.setLevel(logging.INFO)
-
-        self.logger_statistics.addHandler(self.ch)
-        self.logger_statistics.addHandler(self.fh_statistics)
         self.fh_statistics.setFormatter(formatter)
 
-    def close_logger(self):
-        super().close_logger()
-        for handler in self.logger_statistics.handlers[:]:
-            handler.close()
-            self.logger_statistics.removeHandler(handler)
+        # Add handlers to the logger
+        self.logger_statistics.addHandler(self.ch)
+        self.logger_statistics.addHandler(self.fh_statistics)
+
         
     
     
